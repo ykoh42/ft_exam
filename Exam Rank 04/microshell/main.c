573137e88319c1136cd94e470e73f77bf7948a51
ykoh <ykoh@student.42seoul.kr>
2021-02-26 06:02:05 +0900

    progress

--- Exam Rank 04/microshell/main.c
+++ Exam Rank 04/microshell/main.c
@@ -3,59 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef struct	s_data {
-	int			is_pipe;
-	char		**argv;
-	char		**envp;
-}				t_data;
+int 	std_out;
+int 	std_in;
 
-int			ft_execve(char *path, char *argv[], char *envp[], int is_pipe)
+size_t		ft_strlen(const char *s)
 {
-	int		fd[2];
-	pid_t	pid;
-	int		i;
+	size_t	i;
 
-	if (is_pipe)
-	{
-		if (pipe(fd) < 0)
-		{
-			write(2, "error: fatal\n", 13);
-			exit(1);
-		}
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-			write(2, "error: fatal\n", 13);
-			exit(1);
-	}
-	else if (pid == 0)
-	{
-		if (execve(path, argv, envp) < 0)
-		{
-			write(2, "error: cannot execute ", 22);
-			i = 0;
-			while (path[i])
-				i++;
-			write(2, path, i);
-			write(2, "\n", 1);
-			exit(1);
-		}
-	}
-	else
-	{
-
-
-	}
-
-
-
-
-
-	return (0);
-	path +=0;
-	argv+=0;
-	envp+=0;
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
 int			ft_cd(char *argv[])
@@ -75,10 +33,7 @@ int			ft_cd(char *argv[])
 		if (chdir(argv[1]) < 0)
 		{
 			write(2, "error: cd: cannot change directory to ", 38);
-			i = 0;
-			while (argv[1][i])
-				i++;
-			write(2, argv[1], i);
+			write(2, argv[1], ft_strlen(argv[1]));
 			write(2, "\n", 1);
 			exit(1);
 		}
@@ -86,53 +41,135 @@ int			ft_cd(char *argv[])
 	return (0);
 }
 
-int			command(char *argv[], char *envp[])
+int			is_pipe(char *argv[])
 {
-	int	is_pipe;
+	int	ret;
 	int i;
 
-	is_pipe = 0;
+	ret = 0;
 	i = 0;
 	while (argv[i])
 	{
 		if (strncmp(argv[i], "|", 2) == 0)
 		{
-			is_pipe = 1;
+			ret = 1;
 			argv[i] = 0;
-			break ;
+			break ; // "|"
 		}
 		else if (strncmp(argv[i], ";", 2) == 0)
 		{
-			is_pipe = 0;
+			ret = 0;
 			argv[i] = 0;
-			break ;
+			break ; // ";"
 		}
 		i++;
 	}
-	if (strncmp(argv[0], "cd", 3) == '\0')
-		ft_cd(argv);
+	return (ret);
+}
+
+/*
+int			ft_execve(char *argv[], char *envp[], int condition, int *fd2)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+			write(2, "error: fatal\n", 13);
+			exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(argv[0], argv, envp) < 0)
+		{
+			write(2, "error: cannot execute ", 22);
+			write(2, argv[0], ft_strlen(argv[0]));
+			write(2, "\n", 1);
+			exit(1);
+		}
+	}
 	else
-	 	ft_execve(argv[0], argv, envp, is_pipe);
-	return (i);
+	{
+
+	}
+	return (0);
+}
+*/
+
+void		command(int argc, char *argv[], char *envp[], int *fd_before)
+{
+	int 	i;
+	int		condition;
+	pid_t	pid;
+	int 	fd_new[2];
+	// 종료 조건
+	if (argc <= 1)
+		return ;
+	// pipe 인지 확인하고 NULL 삽입
+	condition = is_pipe(argv);
+	// printf("condition : %d\n", condition);
+	dup2(fd_before[0], 0);
+	close(fd_before[0]);
+	close(fd_before[1]);
+	fd_new[0] = 0;
+	fd_new[1] = 1;
+	if (condition)
+	{
+		pipe(fd_new);
+		// close(fd_new[0]);
+		dup2(fd_new[1], 1);
+		close(fd_new[1]);
+	}
+	else
+	{
+		dup2(std_out, 1);
+	}
+	
+	pid = fork();
+	if (pid == 0)
+	{
+		// dup2(fd_before[1], 1);
+		// dup2(fd_new[1], 1);
+		// if (fd_before[0] != 0)
+		// 	close(fd_before[0]);
+		// if (fd_before[1] != 1)
+		// 	close(fd_before[1]);
+		if (execve(argv[0], argv, envp) < 0)
+		{
+			write(2, "error: cannot execute\n", 22);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, 0, 0);
+	}
+
+	i = 0;
+	while (argv[i])
+		i++;
+
+	// 다음껄로 넘어간 다음 다시 command 호출, argc 갯수 초과했는지 봐야할듯
+	command(argc - 1 - i, argv + 1 + i, envp, fd_new);
 }
 
 int			main(int argc, char *argv[], char *envp[])
 {
-	int		i;
+	int	fd[2];
 
+	std_in = dup(0);
+	std_out = dup(1);
 	argv++;
-	i = 0;
-	while (i < argc - 1)
+	if (pipe(fd) < 0)
 	{
-
-		// printf("argv[%d] : %s\n",i, argv[i]);
-		i += command(argv + i, envp);
-		i++;
+		write(2, "error: fatal\n", 13);
+		exit(1);
 	}
 
-
-
-
+	// printf("before : %d %d\n", fd1[0], fd1[1]);
+	command(argc, argv, envp, fd);
+	// printf("after  : %d %d\n", fd1[0], fd1[1]);
+	while (1);
+		// dup2(std_in, 0);
 	return (0);
-	envp+=0;
 }

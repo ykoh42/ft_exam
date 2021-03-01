6d2564241d77d62ff535e8326c6615f10a15f06a
Ykoh <ykoh@student.42seoul.kr>
2021-03-01 15:39:43 +0900

    Add: microshell.c

---
+++ Exam Rank 04/microshell/microshell.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#ifdef TEST_SH
+# define TEST		1
+#else
+# define TEST		0
+#endif
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int			ft_cd(char *argv[])
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+		i++;
+	if (i != 2)
+	{
+		write(2, "error: cd: bad arguments\n", 25);
+		return (-1);
+	}
+	else
+	{
+		if (chdir(argv[1]) < 0)
+		{
+			write(2, "error: cd: cannot change directory to ", 38);
+			write(2, argv[1], ft_strlen(argv[1]));
+			write(2, "\n", 1);
+			return (-1);
+		}
+	}
+	return (0);
+}
+
+int			is_pipe(char *argv[])
+{
+	int	ret;
+	int i;
+
+	ret = 0;
+	i = 0;
+	while (argv[i])
+	{
+		if (strncmp(argv[i], "|", 2) == 0)
+		{
+			ret = 1;
+			argv[i] = 0;
+			break ;
+		}
+		else if (strncmp(argv[i], ";", 2) == 0)
+		{
+			ret = 0;
+			argv[i] = 0;
+			break ;
+		}
+		i++;
+	}
+	return (ret);
+}
+
+void		command(int argc, char *argv[], char *envp[], int prev, int *fd_prev)
+{
+	int		exit_status;
+	int 	i;
+	int		next;
+	int 	fd_next[2];
+	pid_t	pid;
+
+	if (argc <= 1)
+		return ;
+	next = is_pipe(argv);
+	if (argv[0] && next)
+	{
+		if (pipe(fd_next) < 0)
+		{
+			write(2, "error fatal\n", 12);
+			exit(1);
+		}
+	}
+	if (argv[0])
+	{
+		if (strncmp(argv[0], "cd", 3) == 0)
+			ft_cd(argv);
+		else if ((pid = fork()) < 0)
+		{
+			write(2, "error fatal\n", 12);
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			if (prev)
+			{
+				if (dup2(fd_prev[0], 0) < 0)
+				{
+					write(2, "error fatal\n", 12);
+					exit(-1);
+				}
+			}
+			if (next)
+			{
+				if (dup2(fd_next[1], 1) < 0)
+				{
+					write(2, "error fatal\n", 12);
+					exit(-1);
+				}
+			}
+			if (execve(argv[0], argv, envp) < 0)
+			{
+				write(2, "error: cannot execute ", 22);
+				write(2, argv[0], ft_strlen(argv[0]));
+				write(2, "\n", 1);
+			}
+		}
+		else
+		{
+			waitpid(pid, &exit_status, 0);
+			if (exit_status / 256 == 255)
+				exit(1);
+			if (prev)
+				close(fd_prev[0]);
+			if (next)
+				close(fd_next[1]);
+		}
+	}
+	i = 0;
+	while (argv[i])
+		i++;
+	command(argc - 1 - i, argv + 1 + i, envp, next, fd_next);
+}
+
+int			main(int argc, char *argv[], char *envp[])
+{
+	int	prev;
+	int	fd_prev[2];
+	
+	prev = 0;
+	fd_prev[0] = 0;
+	fd_prev[1] = 1;
+	command(argc, argv + 1, envp, prev, fd_prev);
+	// while (1);
+
+	if (TEST)
+		while (1);
+	return (0);
+}
+
